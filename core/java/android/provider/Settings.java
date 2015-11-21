0f393b0c9b779ce4a3b9ba6ab9294abf8db241fe
maxwen <max.weninger@gmail.com>
2015-11-21 02:00:46 +0000

    Add support for adb over network [1/2]
    Change-Id: Ifbed5500178a16d47203af6c8c7e5fb35d42bb88
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6992,6 +6992,13 @@ public final class Settings {
          */
         public static final String THEME_PREV_BOOT_API_LEVEL = "theme_prev_boot_api_level";
 
+
+        /**
+         * The TCP/IP port to run ADB on, or -1 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
         /**
          * @hide
          */
